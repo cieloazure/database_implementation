df57318ff26f45fb4f4ea38b61683547da56981e
Akash S <akash.shingte@gmail.com>
2019-03-21 00:51:23 -0400

    Refactored the entire project

--- lib/include/ParseTree.h
+++ lib/include/ParseTree.h
@@ -12,45 +12,37 @@
 #define NAME 4
 
 struct Operand {
+  // this tells us the type of the operand: FLOAT, INT, STRING...
+  int code;
 
-	// this tells us the type of the operand: FLOAT, INT, STRING...
-	int code;
-
-	// this is the actual operand
-	char *value;
+  // this is the actual operand
+  char *value;
 };
 
 struct ComparisonOp {
+  // this corresponds to one of the codes describing what type
+  // of literal value we have in this nodes: LESS_THAN, EQUALS...
+  int code;
 
-	// this corresponds to one of the codes describing what type
-	// of literal value we have in this nodes: LESS_THAN, EQUALS...
-	int code;
-
-	// these are the operands on the left and on the right
-	struct Operand *left;
-	struct Operand *right;	
+  // these are the operands on the left and on the right
+  struct Operand *left;
+  struct Operand *right;
 };
 
 struct OrList {
+  // this is the comparison to the left of the OR
+  struct ComparisonOp *left;
 
-	// this is the comparison to the left of the OR
-	struct ComparisonOp *left;
-
-	// this is the OrList to the right of the OR; again,
-	// this might be NULL if the right is a simple comparison
-	struct OrList *rightOr;
+  // this is the OrList to the right of the OR; again,
+  // this might be NULL if the right is a simple comparison
+  struct OrList *rightOr;
 };
 
 struct AndList {
+  // this is the disjunction to the left of the AND
+  struct OrList *left;
 
-	// this is the disjunction to the left of the AND
-	struct OrList *left;
-
-	// this is the AndList to the right of the AND
-	// note that this can be NULL if the right is a disjunction
-	struct AndList *rightAnd;
-
+  // this is the AndList to the right of the AND
+  // note that this can be NULL if the right is a disjunction
+  struct AndList *rightAnd;
 };
-
-
-
