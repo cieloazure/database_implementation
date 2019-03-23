b224a7eaf2294b182ee7dad6391a3e9b36413c8c
Akash S <akash.shingte@gmail.com>
2019-03-23 05:10:00 -0400

    Removed gtest dist

--- dist/googletest/build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/akashshingte/Projects/Cpp/DBI - Assignment 1/P1/googletest/googletest
-# Build directory: /Users/akashshingte/Projects/Cpp/DBI - Assignment 1/P1/googletest/build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
